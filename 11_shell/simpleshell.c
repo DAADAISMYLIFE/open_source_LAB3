3de25b2913983438fa0cb57a840bca4ee49f788f
qkekdhd <qkekdhd@naver.com>
2024-11-29 01:31:43 +0900

    shell 프로그램 리다이렉션 기능 추가

--- 11_shell/simpleshell.c
+++ 11_shell/simpleshell.c
@@ -2,7 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 // 명령어와 인자를 구분하는 함수
 int getargs(char *cmd, char **argv);
 
@@ -12,8 +14,10 @@ int main(){
 	int narg;
 	pid_t pid;
 	int is_background;
+	int is_error;
 
 	while(1){
+		is_error = 0;
 		// 사용자 명령 입력 
 		printf("shell> ");
 		gets(buf);
@@ -30,9 +34,65 @@ int main(){
 		// 2. 마지막 인자가 &이면 백그라운드 작업해야함
 		// 	- 인자하나 없얘야함.
 		if(strcmp(argv[narg - 1], "&") == 0){
-			argv[narg-1] = NULL;
+			argv[narg--] = NULL;
 			is_background = 1;
 		}
+
+
+		// 3. 리다이렉션 및 파이프
+		int i;
+		int fd = -1;
+		
+		// 백업용 fd
+		int fd_in = dup(0);
+		int fd_out = dup(1);
+		int fd_err = dup(2);
+		
+		for(i = 0; i < narg; i++){
+			if(strcmp(argv[i], ">") == 0){	
+				if((fd = open(argv[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1){
+					perror("open");
+					exit(1);
+				}
+				if((dup2(fd, 1) == -1)){
+					perror("dup2");
+					exit(1);
+				}
+				argv[i] = NULL;
+				break;
+			}
+			else if(strcmp(argv[i], ">>") == 0){
+				if((fd = open(argv[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644)) == -1){
+					perror("open");
+					exit(1);
+				}
+				if((dup2(fd, 1) == -1)){
+					perror("dup2");
+					exit(1);
+				}
+				argv[i] = NULL;
+				break;
+			}	
+			else if(strcmp(argv[i], "<") == 0){
+				if((fd = open(argv[i + 1], O_RDONLY, 0644)) == -1){
+					perror("open");
+					is_error = 1;
+					break;
+				}
+				if((dup2(fd, 0) == -1)){
+					perror("dup2");
+					exit(1);
+				}
+				argv[i] = NULL;
+				break;
+			}
+		}
+		
+		// 혹시 이전에 에러나오면 명령어 수행하지 말기
+		if(is_error == 1)
+			continue;
+
+		// 명령어 수행 과정 
 		// 자식 프로세스 생성
 		pid = fork();
 
@@ -40,6 +100,8 @@ int main(){
 			// 자식 프로세스는 명령어 수행
 			// 명령어 수행
 			execvp(argv[0], argv);
+			perror("명령어 수행 불가");
+			exit(1);
 		}
 		else if(pid > 0){
 
@@ -53,6 +115,21 @@ int main(){
 		else{
 			perror("fork failed");
 		}
+		
+		// 리다이렉션 후 원래 파일 복구 및  파일 디스크립터 닫기
+		if (fd != -1){
+			
+			if((dup2(fd_in, 0) == -1) || (dup2(fd_out, 1) == -1) || (dup2(fd_err, 2) == -1)){
+                        	perror("dup2");
+                        	exit(1);
+                        }
+
+			if((close(fd) == -1) || (close(fd_in) == -1) || (close(fd_out) == -1) || (close(fd_err) == -1)) {
+    				perror("close");
+    				exit(1);
+			}
+		}		
+
 	}
 }
 
